6cf8420564e8701923398444f0492fccee84852d
marcosvisentini <marcosvisentini@gmail.com>
2021-02-18 21:49:58 -0300

    Add 'HUMAN vs COMPUTER' game mode

--- main.c
+++ main.c
@@ -12,6 +12,8 @@ typedef struct {
     int number_of_moves;
 } player;
 
+int chosen_game_mode = 0;
+
 void initialize_board(char board[BOARD_SIZE][BOARD_SIZE])
 {
     for (int i = 0; i < BOARD_SIZE; i++) {
@@ -43,6 +45,31 @@ void clear_stream(void)
     }
 }
 
+void sleep_ms(int milliseconds)
+{
+    #ifdef WIN32
+        Sleep(milliseconds);
+    #elif _POSIX_C_SOURCE >= 199309L
+        struct timespec ts;
+        ts.tv_sec = milliseconds / 1000;
+        ts.tv_nsec = (milliseconds % 1000) * 1000000;
+        nanosleep(&ts, NULL);
+    #else
+        usleep(milliseconds * 1000);
+    #endif
+}
+
+void print_players_info(player players[NUMBER_OF_PLAYERS])
+{
+    if (chosen_game_mode == 1) {
+        printf("Player 1, you are %c[%d;%dm %c %c[0m\n", 27, 30, 41, players[0].piece, 27);
+        printf("Player 2, you are %c[%d;%dm %c %c[0m\n\n", 27, 30, 44, players[1].piece, 27);
+    } else {
+        printf("Human, you are   %c[%d;%dm %c %c[0m\n", 27, 30, 41, players[0].piece, 27);
+        printf("Computer is      %c[%d;%dm %c %c[0m\n\n", 27, 30, 44, players[1].piece, 27);
+    }
+}
+
 void initial_settings(player players[NUMBER_OF_PLAYERS])
 {
     players[0].id = 1;
@@ -53,9 +80,8 @@ void initial_settings(player players[NUMBER_OF_PLAYERS])
     players[1].piece = 'O';
     players[1].number_of_moves = 0;
 
-    printf("Player 1, you are %c[%d;%dm %c %c[0m.\n", 27, 30, 41, players[0].piece, 27);
-    printf("Player 2, you are %c[%d;%dm %c %c[0m.\n\n", 27, 30, 44, players[1].piece, 27);
-
+    print_players_info(players);
+    clear_stream();
     press_enter();
 }
 
@@ -88,7 +114,6 @@ void print_cell(char cell)
     change_color(cell);
     printf(" %c ", cell);
     reset_color();
-    printf("|");
 }
 
 void print_board(char board[BOARD_SIZE][BOARD_SIZE])
@@ -98,6 +123,7 @@ void print_board(char board[BOARD_SIZE][BOARD_SIZE])
         printf("\t|");
         for (int j = 0; j < BOARD_SIZE; j++) {
             print_cell(board[i][j]);
+            printf("|");
         }
         printf("\n\t+---+---+---+\n");
     }
@@ -196,10 +222,17 @@ void tie_message()
 
 void win_message(int player_id, char piece, int number_of_moves)
 {
-    printf("\n\nPlayer %d [", player_id);
-    change_color(piece);
-    printf(" %c ", piece);
-    reset_color();
+    if (chosen_game_mode == 1) {
+        printf("\n\nPlayer %d [", player_id);
+    } else {
+        if (player_id == 1) {
+            printf("\n\nHuman [");
+        } else {
+            printf("\n\nComputer [");
+        }
+    }
+
+    print_cell(piece);
     printf("] won with %d moves! \\o/\n", number_of_moves);
 }
 
@@ -238,7 +271,7 @@ bool play_again(void)
             case 'N':
                 return false;
             default:
-                printf("\nInvalid choice input. Try again...\n");
+                printf("\nInvalid input. Try again...\n");
                 continue;
         }
     }
@@ -246,8 +279,6 @@ bool play_again(void)
 
 void greeting(void)
 {
-    printf("\n\nHey folks, let's play Tick Tack Toe \\o/\n\n");
-
     char board[BOARD_SIZE][BOARD_SIZE] = {
         {'1', '2', '3'},
         {'4', '5', '6'},
@@ -260,10 +291,17 @@ void greeting(void)
 
 void player_message(int player_id, char piece)
 {
-    printf("\n\nPlayer %d, you are ", player_id);
-    change_color(piece);
-    printf(" %c ", piece);
-    reset_color();
+    if (chosen_game_mode == 1) {
+        printf("\nPlayer %d, you are ", player_id);
+    } else {
+        if (player_id == 1) {
+            printf("\nHuman, you are ");
+        } else {
+            printf("\nComputer is ");
+        }
+    }
+
+    print_cell(piece);
     printf(".\n");
 }
 
@@ -272,11 +310,14 @@ void player_move(char piece, char board[BOARD_SIZE][BOARD_SIZE])
     while (true) {
         int cell, row, column;
 
-        printf("\nEnter an empty cell [1 - 9] you want to place the %c on: ", piece);
+        printf("\nEnter an empty cell [1 - 9] you want to place the ");
+        print_cell(piece);
+        printf(" on: ");
+
         scanf("%d", &cell);
 
         if (cell < 1 || cell > (BOARD_SIZE * BOARD_SIZE)) {
-            printf("\n\nInvalid cell input. Try again...\n\n");
+            printf("\n\nInvalid input. Try again...\n\n");
             continue;
         }
 
@@ -284,7 +325,7 @@ void player_move(char piece, char board[BOARD_SIZE][BOARD_SIZE])
         column = (cell - 1) % 3;
 
         if (board[row][column] != ' ') {
-            printf("\n\nThe input cell is already filled. Try again...\n\n");
+            printf("\n\nThe entered cell is already filled. Try again...\n\n");
             continue;
         }
 
@@ -293,24 +334,61 @@ void player_move(char piece, char board[BOARD_SIZE][BOARD_SIZE])
     }
 }
 
-int who_plays_first(player players[NUMBER_OF_PLAYERS])
+void computer_move(char piece, char board[BOARD_SIZE][BOARD_SIZE])
+{
+    while (true) {
+        int row = rand() % BOARD_SIZE;
+        int column = rand() % BOARD_SIZE;
+
+        if (board[row][column] != ' ') {
+            continue;
+        }
+
+        board[row][column] = piece;
+        break;
+    }
+}
+
+int who_plays_first(void)
 {
     int first_player_index = (int)(random() % 2);
 
-    printf("\nPlayer %d plays first! \\o/\n", players[first_player_index].id);
+    if (chosen_game_mode == 1) {
+        printf("\nPlayer %d plays first! \\o/\n", first_player_index + 1);
+    } else {
+        if (first_player_index == 0) {
+            printf("\nHuman plays first! \\o/\n");
+        } else {
+            printf("\nComputer plays first! \\o/\n");
+        }
+    }
 
     return first_player_index;
 }
 
 void gameplay(player players[NUMBER_OF_PLAYERS], char board[BOARD_SIZE][BOARD_SIZE])
 {
-    int player_index = who_plays_first(players);
+    int player_index = who_plays_first();
     char piece = players[player_index].piece;
+
+    print_board(board);
 
     while (true) {
         player_message(player_index + 1, piece);
+
+        if (chosen_game_mode == 1) {
+            print_board(board);
+            player_move(piece, board);
+        } else {
+            if (player_index == 0) {
+                player_move(piece, board);
+            } else {
+                computer_move(piece, board);
+                sleep_ms((random() % (2000 - 1000 + 1)) + 1000);
+            }
+        }
+        
         print_board(board);
-        player_move(piece, board);
 
         int number_of_moves = ++players[player_index].number_of_moves;
 
@@ -323,6 +401,26 @@ void gameplay(player players[NUMBER_OF_PLAYERS], char board[BOARD_SIZE][BOARD_SI
     }
 }
 
+void game_mode(void)
+{
+    printf("\n\nHey folks, let's play Tick Tack Toe \\o/\n\n");
+
+    while (true) {
+        printf("\nChoose a game mode:\n");
+        printf("\n1. PLAYER 1  vs  PLAYER 2");
+        printf("\n2. HUMAN     vs  COMPUTER\n\n");
+        scanf("%d", &chosen_game_mode);
+ 
+        if (chosen_game_mode < 1 || chosen_game_mode > 2) {
+            printf("\nInvalid input. Try again...\n");
+            continue;
+        }
+ 
+        greeting();
+        break;
+    }
+}
+
 int main(void)
 {
     srand(time(NULL));
@@ -331,7 +429,7 @@ int main(void)
         char board[BOARD_SIZE][BOARD_SIZE];
         player players[NUMBER_OF_PLAYERS];
 
-        greeting();
+        game_mode();
         initial_settings(players);
         initialize_board(board);
         gameplay(players, board);
